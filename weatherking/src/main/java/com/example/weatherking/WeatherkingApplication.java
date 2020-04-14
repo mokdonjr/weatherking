b5dd4f79b1cbf7cd4242b6a2b817393b7fe2cdb7
baegseungchan-gram <mokdonjr@gmail.com>
2020-04-15 02:33:49 +0900

    initialize spring boot project

---
+++ weatherking/src/main/java/com/example/weatherking/WeatherkingApplication.java
@@ -0,0 +1,13 @@
+package com.example.weatherking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherkingApplication.class, args);
+	}
+
+}
---
+++ weatherking/src/test/java/com/example/weatherking/WeatherkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weatherking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
