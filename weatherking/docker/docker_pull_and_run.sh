533ac04d2fa678cec5f15d10d153ebb9ff6865d1
baegseungchan-gram <mokdonjr@gmail.com>
2020-04-21 02:55:28 +0900

    [배포] 개발 DB 세팅, 스탠드얼론 환경 도커컴포즈 작성

--- weatherking/docker/docker_pull_and_run.sh
+++ weatherking/docker/docker_pull_and_run.sh
@@ -5,10 +5,14 @@ DOCKER_IMAGE_NAME=$2
 SPRING_PROFILE=$3
 MYSQL_ROOT_PASSWORD=$4
 
-sed -i 's/DOCKER_REPOSITORY=/DOCKER_REPOSITORY=${DOCKER_REPOSITORY}/g' docker-compose.env
-sed -i 's/DOCKER_IMAGE_NAME=/DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}/g' docker-compose.env
-sed -i 's/SPRING_PROFILE=/SPRING_PROFILE=${SPRING_PROFILE}/g' docker-compose.env
-sed -i 's/MYSQL_ROOT_PASSWORD=/MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}/g' docker-compose.env
+echo "DOCKER_REPOSITORY=${DOCKER_REPOSITORY}"
+sed -i "s/DOCKER_REPOSITORY=/DOCKER_REPOSITORY=${DOCKER_REPOSITORY}/g" docker-compose.env
+echo "DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}"
+sed -i "s/DOCKER_IMAGE_NAME=/DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}/g" docker-compose.env
+echo "SPRING_PROFILE=${SPRING_PROFILE}"
+sed -i "s/SPRING_PROFILE=/SPRING_PROFILE=${SPRING_PROFILE}/g" docker-compose.env
+echo "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}"
+sed -i "s/MYSQL_ROOT_PASSWORD=/MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}/g" docker-compose.env
 
 #docker-compose -f docker-compose.yml pull spring_app
-docker-compose up -d
+#docker-compose up -d
