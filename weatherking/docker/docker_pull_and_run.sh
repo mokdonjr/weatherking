5ce39ec90a9e57e8bf0d0f3780e1b776b3ba22b8
baegseungchan-gram <mokdonjr@gmail.com>
2020-04-21 02:49:45 +0900

    [배포] 개발 DB 세팅, 스탠드얼론 환경 도커컴포즈 작성

--- weatherking/docker/docker_pull_and_run.sh
+++ weatherking/docker/docker_pull_and_run.sh
@@ -5,5 +5,10 @@ DOCKER_IMAGE_NAME=$2
 SPRING_PROFILE=$3
 MYSQL_ROOT_PASSWORD=$4
 
+sed -i 's/DOCKER_REPOSITORY=/DOCKER_REPOSITORY=${DOCKER_REPOSITORY}/g' docker-compose.env
+sed -i 's/DOCKER_IMAGE_NAME=/DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}/g' docker-compose.env
+sed -i 's/SPRING_PROFILE=/SPRING_PROFILE=${SPRING_PROFILE}/g' docker-compose.env
+sed -i 's/MYSQL_ROOT_PASSWORD=/MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}/g' docker-compose.env
+
 #docker-compose -f docker-compose.yml pull spring_app
 docker-compose up -d
