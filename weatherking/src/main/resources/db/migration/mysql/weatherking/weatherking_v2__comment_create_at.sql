badedd49f24895c68ea1a40813be7759f0ec5bc3
baegseungchan-gram <mokdonjr@gmail.com>
2020-04-22 02:17:03 +0900

    리눅스 서버 DB 대소문자 구분 대응

--- weatherking/src/main/resources/db/migration/mysql/weatherking/weatherking_v2__comment_create_at.sql
+++ weatherking/src/main/resources/db/migration/mysql/weatherking/weatherking_v2__comment_create_at.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `comment`
+ALTER TABLE `COMMENT`
 ADD COLUMN `is_updated` TINYINT(1) NOT NULL DEFAULT 0 AFTER `create_at`,
 ADD COLUMN `update_at` DATETIME NULL AFTER `is_updated`,
 ADD COLUMN `is_deleted` TINYINT(1) NOT NULL DEFAULT 0 AFTER `update_at`,
