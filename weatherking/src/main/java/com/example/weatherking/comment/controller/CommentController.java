76fc6cccf79ce2d51bf20bd46e288a35758b7f61
baegseungchan-gram <mokdonjr@gmail.com>
2020-04-21 23:38:38 +0900

    [api] 응답 폼 ResponseEntity 적용

--- weatherking/src/main/java/com/example/weatherking/comment/controller/CommentController.java
+++ weatherking/src/main/java/com/example/weatherking/comment/controller/CommentController.java
@@ -1,68 +1,77 @@
 package com.example.weatherking.comment.controller;
 
 import com.example.weatherking.BaseBean;
+import com.example.weatherking.comment.data.Comment;
 import com.example.weatherking.comment.service.CommentService;
-import com.example.weatherking.util.JsonUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * 댓글 요청 수신
  */
 @RequestMapping("/comments")
 @RestController
+@Api(tags = "댓글")
 public class CommentController extends BaseBean {
 
     @Autowired
     private CommentService commentService;
 
     @PostMapping("/")
-    public String createComment(@RequestParam("message") String message) {
-        commentService.createComment(message);
-        // TODO : 뭘 응답? RESULT 형식이 있어야할듯
-        return "success";
+    @ApiOperation(value = "댓글 작성")
+    public ResponseEntity<Comment> createComment(@RequestParam("message") String message) {
+        var comment = commentService.createComment(message);
+        if (comment == null) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return new ResponseEntity<>(comment, HttpStatus.CREATED);
     }
 
     @GetMapping("/")
-    public String getCommentList() throws JSONException {
+    @ApiOperation(value = "댓글 리스트 조회")
+    public ResponseEntity<List<Comment>> getCommentList() throws JSONException {
         // TODO : 페이징
         var list = commentService.getCommentList();
-        if (list == null)
-            return "no element";
-        // TODO : RESULT 형식이 있어야할듯
-        return JsonUtil.prettyIndentJson(list);
+        if (list == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/{commentId}")
-    public String getComment(@PathVariable(value = "commentId") long commentId) throws JSONException {
+    @ApiOperation(value = "특정 댓글 조회")
+    public ResponseEntity<Comment> getComment(@PathVariable(value = "commentId") long commentId) throws JSONException {
         var comment = commentService.getComment(commentId);
-        // TODO : RESULT 형식이 있어야할듯
-        return JsonUtil.prettyIndentJson(comment);
+        return new ResponseEntity<>(comment, HttpStatus.OK);
     }
 
     @PutMapping("/{commentId}")
-    public String updComment(@PathVariable(value = "commentId") long commentId, @RequestParam("message") String message) {
+    @ApiOperation(value = "댓글 수정")
+    public ResponseEntity<Comment> updComment(@PathVariable(value = "commentId") long commentId, @RequestParam("message") String message) {
         var comment = commentService.getCommentForUpdate(commentId);
         if (comment == null) {
-            // TODO : 뭘 응답? RESULT 형식이 있어야할듯
-            return "no element";
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         comment.setMessage(message);
         commentService.updComment(comment);
-        // TODO : 뭘 응답? RESULT 형식이 있어야할듯
-        return "success";
+        return new ResponseEntity<>(comment, HttpStatus.ACCEPTED);
     }
 
     @DeleteMapping("/{commentId}")
-    public String delComment(@PathVariable(value = "commentId") long commentId) {
+    @ApiOperation(value = "댓글 삭제")
+    public ResponseEntity<?> delComment(@PathVariable(value = "commentId") long commentId) {
         var comment = commentService.getCommentForUpdate(commentId);
         if (comment == null) {
-            // TODO : 뭘 응답? RESULT 형식이 있어야할듯
-            return "no element";
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
         commentService.delComment(commentId);
-        // TODO : 뭘 응답? RESULT 형식이 있어야할듯
-        return "success";
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 }
--- weatherking/src/main/java/com/example/weatherking/weather/controller/WeatherController.java
+++ weatherking/src/main/java/com/example/weatherking/weather/controller/WeatherController.java
@@ -1,32 +1,54 @@
 package com.example.weatherking.weather.controller;
 
 import com.example.weatherking.BaseBean;
-import com.example.weatherking.util.JsonUtil;
+import com.example.weatherking.vfinfo.data.response.VFResponseDefault;
 import com.example.weatherking.weather.service.WeatherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.configurationprocessor.json.JSONException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/weather")
 @RestController
+@Api(tags = "날씨")
 public class WeatherController extends BaseBean {
 
     @Autowired
     private WeatherService weatherService;
 
     @GetMapping("/normal")
-    public String weatherNormal(@RequestParam("nx") int nx, @RequestParam("ny") int ny) throws JSONException {
-        return JsonUtil.prettyIndentJson(weatherService.getWeatherNormalByLocation(nx, ny));
+    @ApiOperation(value = "동네예보조회")
+    public ResponseEntity<VFResponseDefault> weatherNormal(@RequestParam("nx") int nx, @RequestParam("ny") int ny) throws JSONException {
+        var response = weatherService.getWeatherNormalByLocation(nx, ny);
+        if (response == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/forecast")
-    public String weatherForecast(@RequestParam("nx") int nx, @RequestParam("ny") int ny) throws JSONException {
-        return JsonUtil.prettyIndentJson(weatherService.getWeatherUltraForecastByLocation(nx, ny));
+    @ApiOperation(value = "초단기예보조회")
+    public ResponseEntity<VFResponseDefault> weatherForecast(@RequestParam("nx") int nx, @RequestParam("ny") int ny) throws JSONException {
+        var response = weatherService.getWeatherUltraForecastByLocation(nx, ny);
+        if (response == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/current")
-    public String weatherCurrent(@RequestParam("nx") int nx, @RequestParam("ny") int ny) throws JSONException {
-        return JsonUtil.prettyIndentJson(weatherService.getWeatherUltraCurrentByLocation(nx, ny));
+    @ApiOperation(value = "초단기실황조회")
+    public ResponseEntity<VFResponseDefault> weatherCurrent(@RequestParam("nx") int nx, @RequestParam("ny") int ny) throws JSONException {
+        var response = weatherService.getWeatherUltraCurrentByLocation(nx, ny);
+        if (response == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
