f78d1cf2e0f2de885598d0699e61c74283a5e0b9
baegseungchan-gram <mokdonjr@gmail.com>
2020-04-25 03:26:59 +0900

    프로토콜 변경 작업으로 인한 안내문

--- weatherking/src/main/java/com/example/weatherking/HomeController.java
+++ weatherking/src/main/java/com/example/weatherking/HomeController.java
@@ -3,13 +3,19 @@ package com.example.weatherking;
 import io.swagger.annotations.Api;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @Api(tags = "API 조회")
 public class HomeController extends BaseBean {
 
+//    @GetMapping("/")
+//    public String home() {
+//        return "redirect:/swagger-ui.html";
+//    }
+
     @GetMapping("/")
-    public String home() {
-        return "redirect:/swagger-ui.html";
+    public @ResponseBody String home() {
+        return "<h3>프로토콜 변경 작업중. 임시 폐쇄</h3><p>https://github.com/mokdonjr/weatherking-server/commits/master</p> ";
     }
 }
